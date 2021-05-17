4eb9fb35ff23a620d6d5a7656b0735ca9fa619ae
Gabriela de Mesquita Sampaio Duarte <gabrielad4@al.insper.edu.br>
2021-05-17 12:31:36 -0300

    arrumando a ordem das funções

---
+++ tamanho&cores.py
@@ -0,0 +1,5 @@
+# teste de tamanhos para o jogo:
+import pygame
+dimensão = (400, 500)
+tela = pygame.display.set_mode(dimensão)
+pygame.display.set_caption('Tetris') # não necessariamente vai chamar assim, mas como é uma tentativa de replicar o tetris original vamos começar testando com esse nome
