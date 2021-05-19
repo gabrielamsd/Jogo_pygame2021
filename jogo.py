d4379fb8677728aab806173650e4a48d60f32d91
Gabriela de Mesquita Sampaio Duarte <gabrielad4@al.insper.edu.br>
2021-05-19 10:19:59 -0300

    criando uma classe com os parametros de jogo

---
+++ jogo.py
@@ -0,0 +1,10 @@
+#definindo a classe jogo
+class Jogo:
+    campo = []
+    nível = #começa definindo o nível do jogo que aparece na tela, pode ser um único, só determina a dificuldade do jogo
+    pontuação = #eu diria que é legal começar do zero
+    zoom = 
+    altura = x
+    largura = y
+    situação = 'start'
+
